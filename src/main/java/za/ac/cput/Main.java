bb4d1925e2f16bed2618dae07ccb7251c5fb199a
Siyabulela <S.Mgijima@plum.systems>
2025-03-28 19:18:52 +0200

    AccountFactory

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -1,19 +1,34 @@
 package za.ac.cput;
 
+import za.ac.cput.Domain.Account;
 import za.ac.cput.Domain.Address;
 import za.ac.cput.Domain.Contact;
 import za.ac.cput.Domain.Customer;
+import za.ac.cput.factory.AccountFactory;
+import za.ac.cput.factory.AddressFactory;
+import za.ac.cput.factory.ContactFactory;
+import za.ac.cput.factory.CustomerFactory;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Address add = new Address.Builder().setHouseNumber("10").build();
-        System.out.println(add);
-        Contact contact = new Contact.Builder().setEmail("ttt").build();
-        System.out.println(contact);
+        // Creating a Customer instance
+        Customer cus3 = CustomerFactory.createCustomer(
+                "Thabiso", "Mbatha",
+                AddressFactory.createShortAddress("10", "Dorset", "Woodstock"),
+                ContactFactory.createContact("0761234567", "222222222@mycput.ac.za")
+        );
 
-        Customer cus1 = new Customer.Builder().setCustomerName("Thabiso").setCustomerSurname("Mbatha").setCustomerAddress(new Address.Builder().setHouseNumber("10").setStreetName("Dorset").setSuburb("Woodstock").setCity("Cape Town").setPostalCode(1234).setCountry("Mzansi").build()).setCustomerContact(new Contact.Builder().setPhoneNumber("0761234567").setEmail("222222222@mycput.ac.za").build()).build();
+        // Correct way to create LocalDate
+        LocalDate openDate = LocalDate.of(2022, 8, 10);
 
-        System.out.println(cus1);
-        System.out.println(cus1);
+        // Creating an Account instance
+        Account account1 = AccountFactory.createAccount("12345678", 5000.00, openDate, cus3);
+
+        // Printing results
+        System.out.println(cus3);
+        System.out.println(account1);
     }
 }
+
--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,32 +1,26 @@
 package za.ac.cput.util;
 
-import org.apache.commons.validator.*;
-
+import java.time.LocalDate;
+import za.ac.cput.Domain.Customer;
 
 public class Helper {
-    public static boolean stringIsNullOrEmpty(String s) {
-        if (s.isEmpty() || s == null) {
+
+    public static boolean stringIsNullOrEmpty(String value) {
+        if(value.isEmpty() || value == null || value.equals("")){
             return true;
         }
         return false;
     }
 
-    public static boolean intIsEqualsToZero(int i) {
-        if (i == 0) {
+    public static boolean intIsEqualsToZero(int value) {
+        if(value == 0){
             return true;
         }
         return false;
     }
 
     public static boolean isValidEmail(String email) {
-        /*EmailValidator validator = EmailValidator.getInstance();
-        if (validator.isValid(email)) {
-            return true;
-        } else {
-            return false;
-        }*/
+        //To-Do
         return true;
     }
-
-
 }
