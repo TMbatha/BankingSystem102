297af1d0c40f5cf1caab5e74def9e71e57d0a95b
Mcatshulwa <222614153@mycput.ac.za>
2025-03-28 13:02:41 +0200

    TransactionFactory POJO

---
+++ src/main/java/za/ac/cput/factory/TransactionFactory.java
@@ -0,0 +1,35 @@
+package za.ac.cput.factory;
+/* Factory for creating Transaction objects
+   Author: 222614153
+*/
+
+
+import za.ac.cput.Domain.Transaction;
+import za.ac.cput.util.Helper;
+
+public class TransactionFactory {
+
+    public static Transaction createDeposit(String transactionId, double amount, String date) {
+        if (Helper.stringIsNullOrEmpty(transactionId) || amount <= 0 || Helper.stringIsNullOrEmpty(date)) {
+            return null;
+        }
+        return new Transaction.Builder()
+                .transactionId(transactionId)
+                .amount(amount)
+                .date(date)
+                .type("DEPOSIT")
+                .build();
+    }
+
+    public static Transaction createWithdrawal(String transactionId, double amount, String date) {
+        if (Helper.stringIsNullOrEmpty(transactionId) || amount <= 0 || Helper.stringIsNullOrEmpty(date)) {
+            return null;
+        }
+        return new Transaction.Builder()
+                .transactionId(transactionId)
+                .amount(amount)
+                .date(date)
+                .type("WITHDRAWAL")
+                .build();
+    }
+}
